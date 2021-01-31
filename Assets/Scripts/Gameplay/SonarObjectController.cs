5b96708642a6b7a06c63cc5a7f506f2e8530521a
Tarcísio Costa <tarcisiotm@gmail.com>
2021-01-31 15:43:13 -0800

    Sonar

---
+++ Assets/Scripts/Gameplay/SonarObjectController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SonarObjectController : MonoBehaviour
+{
+    [SerializeField] private Transform _player;
+    [SerializeField] private Transform _target;
+
+    [SerializeField] private float _deviationFromCenter;
+    [SerializeField] private float _sonarSize = 64;
+    [SerializeField] private float _maxPlayerDist = 50;
+
+    float _distance;
+    float _unitToPixel;
+
+    public Vector3 dir;
+    public Vector3 pos;
+
+    private RectTransform _rectTransform;
+
+    void Awake()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+        _unitToPixel = _sonarSize / _maxPlayerDist;
+    }
+
+    void Update()
+    {
+        dir = _target.position - _player.position;
+        _distance = Vector3.Distance(_target.position, _player.position);
+
+        if (_distance > _maxPlayerDist) _distance = _maxPlayerDist;
+
+        _rectTransform.anchoredPosition = _distance * _unitToPixel * new Vector2(dir.normalized.x, dir.normalized.y);
+        pos = _deviationFromCenter * new Vector2(dir.normalized.x, dir.normalized.y);
+    }
+}
---
+++ Assets/Scripts/PulseUIColor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PulseUIColor : MonoBehaviour
+{
+    [SerializeField] float _speed = 2;
+
+    private Image _image;
+
+    void Start()
+    {
+        _image = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        _image.color = new Color(_image.color.r, _image.color.g, _image.color.b, Mathf.PingPong(Time.time * _speed, 1));
+    }
+}
