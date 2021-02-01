faba00636635a6d2861f49557a90168cf76f3164
Tarcísio Costa <tarcisiotm@gmail.com>
2021-01-31 17:26:06 -0800

    Scene updates

---
+++ Assets/Scripts/HUDTextController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
+
+public class HUDTextController : MonoBehaviour
+{
+    [SerializeField] CanvasGroup canvasGroup;
+    [SerializeField] TextMeshProUGUI m_text;
+    [SerializeField] float _fadeDuration = .3f;
+    [SerializeField] float _duration = 5f;
+
+    public void SetText(string text)
+    {
+        m_text.text = text;
+        canvasGroup.DOFade(1, _fadeDuration).OnComplete(FadeOut);
+    }
+
+    void FadeOut()
+    {
+        canvasGroup.DOFade(0, _fadeDuration).SetDelay(_duration);
+    }
+}
