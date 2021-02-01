af5c99eedf3df93fba302e3b8b088154b17bd116
Tarcísio Costa <tarcisiotm@gmail.com>
2021-01-31 17:59:11 -0800

    Fading light

---
+++ Assets/Scripts/FadeLight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class FadeLight : MonoBehaviour
+{
+    [SerializeField] private float _duration;
+
+    public void DoFadeLight()
+    {
+        var light = GetComponent<Light>();
+        var intensity = light.intensity;
+        light.intensity = 0;
+        gameObject.SetActive(true);
+        light.DOIntensity(intensity, _duration);
+    }
+}
