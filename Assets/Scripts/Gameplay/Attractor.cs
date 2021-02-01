cf3761f7cfd15f765e4055da4f9677fb5df76764
Tarcísio Costa <tarcisiotm@gmail.com>
2021-01-31 20:57:20 -0800

    Attractor for Material Collectible

---
+++ Assets/Scripts/Gameplay/Attractor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attractor : MonoBehaviour
+{
+    [SerializeField] float _maxDistance = 0.5f;
+
+    Vector3 _pos;
+    PlayerController _player;
+
+    private void OnEnable()
+    {
+        _player = FindObjectOfType<PlayerController>();
+        _pos = transform.position;
+    }
+
+    void Update()
+    {
+        if (_player == null) return;
+
+        transform.position = Vector3.MoveTowards(transform.position, _player.transform.position, _maxDistance * Time.deltaTime);
+    }
+}
