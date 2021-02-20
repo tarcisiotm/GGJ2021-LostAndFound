f6260b1308327d56eff3d9320482c2bf9e22f2ee
Tarcísio Costa <tarcisiotm@gmail.com>
2021-02-19 20:17:46 -0800

    Bug Fixing

--- Assets/Scripts/GameJamTemplate/Audio/AudioManager.cs
+++ Assets/Scripts/GameJamTemplate/Audio/AudioManager.cs
@@ -7,6 +7,7 @@ using DG.Tweening;
 public class AudioManager : TG.Core.Audio.AudioManagerBase
 {
     [Header("Settings")]
+    [SerializeField] float fadeInTime = 1f;
     [SerializeField] float fadeOutTime = 1f;
 
     protected override void OnSceneIsGoingToLoad(int activeSceneBuildIndex, int newSceneBuildIndex) {
@@ -14,6 +15,16 @@ public class AudioManager : TG.Core.Audio.AudioManagerBase
 
         if(bgmAudioSource != null) {
             bgmAudioSource.DOFade(0, fadeOutTime).SetUpdate(true);
+        }
+    }
+
+    protected override void OnSceneLoaded(int activeSceneBuildIndex, int newSceneBuildIndex)
+    {
+        base.OnSceneLoaded(activeSceneBuildIndex, newSceneBuildIndex);
+
+        if (bgmAudioSource != null)
+        {
+            bgmAudioSource.DOFade(1, fadeInTime).SetUpdate(true);
         }
     }
 }
